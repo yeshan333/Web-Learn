510019f2599b1c24aa7a349ad826ec2a53b7ac36
yeshan333 <1329441308@qq.com>
2020-01-07 15:08:09 +0800

    simple react app

--- react/react-tutorial/src/Table.js
+++ react/react-tutorial/src/Table.js
@@ -53,6 +53,7 @@ const TableBody = props => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td><button onClick={() => props.removeCharacter(index)}>Delete</button></td>
             </tr>
         );
     });
@@ -62,12 +63,15 @@ const TableBody = props => {
 
 class Table extends Component {
     render() {
-        const { characterData } = this.props;
+        const { characterData, removeCharacter } = this.props;
 
         return (
             <table>
                 <TableHeader />
-                <TableBody characterData={characterData} />
+                <TableBody
+                    characterData={characterData}
+                    removeCharacter={removeCharacter}
+                />
             </table>
         );
     }
