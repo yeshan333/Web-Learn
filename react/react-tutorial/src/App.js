2e322e4aaff8c41477c095ea6bf9864736cb412f
yeshan333 <1329441308@qq.com>
2020-01-07 13:03:58 +0800

    React props

--- react/react-tutorial/src/App.js
+++ react/react-tutorial/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const characters = [
+            {
+                'name': 'Charlie',
+                'job': 'Janitor'
+            },
+            {
+                'name': 'Mac',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dee',
+                'job': 'Aspring actress'
+            },
+            {
+                'name': 'Dennis',
+                'job': 'Bartender'
+            }
+        ];
+
+        return (
+            <div className="container">
+                <Table characterData={characters} />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ react/react-tutorial/src/Table.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+
+
+//Class component
+/* class Table extends Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Job</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Charlie</td>
+                        <td>Janitor</td>
+                    </tr>
+                    <tr>
+                        <td>Mac</td>
+                        <td>Bouncer</td>
+                    </tr>
+                    <tr>
+                        <td>Dee</td>
+                        <td>Aspiring actress</td>
+                    </tr>
+                    <tr>
+                        <td>Dennis</td>
+                        <td>Bartender</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+} */
+
+//simple component
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        );
+    });
+
+    return <tbody>{rows}</tbody>;
+}
+
+class Table extends Component {
+    render() {
+        const { characterData } = this.props;
+
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} />
+            </table>
+        );
+    }
+}
+
+export default Table;
