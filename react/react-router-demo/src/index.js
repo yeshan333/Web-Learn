10ee8094cabcbec2930449c7257f90f1b92c2efc
yeshan333 <1329441308@qq.com>
2020-01-28 22:49:52 +0800

    react-router

---
+++ react/react-router-demo/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//import AppRouter from './App'
+import AuthExample from './Auth'
+
+ReactDOM.render(<AuthExample/>, document.getElementById('root'));
